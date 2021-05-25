d9e0e0dc492a84c802eec61b50018f613d05ce97
gesse-fernandes <gesse-fm@hotmail.com>
2021-05-25 19:42:06 -0300

    testando o app

--- PrimeiroApp/app/src/main/java/br/com/gesse/fernandes/martins/MainActivity.java
+++ PrimeiroApp/app/src/main/java/br/com/gesse/fernandes/martins/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.gesse.fernandes.martins;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button botao = (Button) findViewById(R.id.button);
+        TextView texto = (TextView)findViewById(R.id.textView);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                texto.setText("você cicou no botao");
+            }
+        });
     }
 }
