b562dd3c4f20aec2f66dee1705f047b8c894fe85
gesse-fernandes <gesse-fm@hotmail.com>
2021-05-26 00:03:14 -0300

    criando o primeiro aplicativo sorteio app onde seleciono de 1 a 10

--- PrimeiroApp/app/src/main/java/br/com/gesse/fernandes/martins/MainActivity.java
+++ PrimeiroApp/app/src/main/java/br/com/gesse/fernandes/martins/MainActivity.java
@@ -7,21 +7,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button botao = (Button) findViewById(R.id.button);
-        TextView texto = (TextView)findViewById(R.id.textView);
 
-        botao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                texto.setText("você cicou no botao");
-            }
-        });
     }
+    public  void jogar(View view)
+    {
+        TextView escolhido = (TextView)findViewById(R.id.txtEscolhido);
+        int numero= new Random().nextInt(11);
+
+        escolhido.setText("O valor escolhido foi: "+numero);
+    }
+
+
 }
